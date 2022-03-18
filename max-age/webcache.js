023934d4fedd9496e6b120e07c09105caf3c03ad
Lml <1093185442@qq.com>
2022-03-18 13:27:40 +0800

    强缓存和协商缓存

---
+++ max-age/webcache.js
@@ -0,0 +1,24 @@
+
+import path from 'path';
+import Koa from 'koa';
+//  https://www.cnblogs.com/tugenhua0707/p/10807289.html
+//静态资源中间件
+import resource from 'koa-static';
+const app = new Koa();
+const host = 'localhost';
+const port = 7878;
+
+app.use(async (ctx, next) => {
+ // 设置响应头Cache-Control 设置资源有效期为300秒
+  ctx.set({
+    'Cache-Control': 'max-age=30'  
+  });
+  console.log('111')
+  await next();
+});
+
+app.use(resource(path.join(__dirname, './static')));
+
+app.listen(port, () => {
+  console.log(`server is listen in ${host}:${port}`);
+});
