981c437d5fd55efcd0837912059cce0ba3b8c3b7
Lml <1093185442@qq.com>
2022-03-21 15:23:22 +0800

    promise all race 实现

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
-const Promise = require('./promise')
+const Promise = require("./promise");
 
 let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('111')
-    },1000)
-})
+  setTimeout(() => {
+    resolve("111");
+  }, 1000);
+});
 
-p.then(null,err => {
-    console.log(err,'err')
-})
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("2222");
+  }, 2000);
+});
+
+Promise.race([p, p1]).then(
+  (res) => {
+    console.log(res,'2222222222');
+  },
+  (err) => {
+    console.log(err,'1111111111111111');
+  }
+);
--- js/promise.js
+++ js/promise.js
@@ -1,62 +1,130 @@
 function Promise(exector) {
-    this.stats = 'padding';
-    this.value = undefined;
-    this.onResolveCallBacks = [];   // 用于存放成功的回调
-    this.onRejectCallBacks = [];    // 用于存放失败的回调
-    let self = this;
+  this.stats = "padding";
+  this.value = undefined;
+  this.onResolveCallBacks = []; // 用于存放成功的回调
+  this.onRejectCallBacks = []; // 用于存放失败的回调
+  let self = this;
 
-    function resolve(value) {
-        if (self.stats === 'padding') {
-            self.stats = 'fulfilled';
-            self.value = value;
-            self.onResolveCallBacks.forEach(cb => cb())
-        }
+  function resolve(value) {
+    if (self.stats === "padding") {
+      self.stats = "fulfilled";
+      self.value = value;
+      self.onResolveCallBacks.forEach((cb) => cb && cb());
     }
+    return value;
+  }
 
-    function reject(value) {
-        if (self.stats === 'padding') {
-            self.stats = 'rejected';
-            self.value = value;
-            self.onRejectCallBacks.forEach(cb => cb())
-        }
-
+  function reject(value) {
+    if (self.stats === "padding") {
+      self.stats = "rejected";
+      self.value = value;
+      self.onRejectCallBacks.forEach((cb) => cb && cb());
     }
+    return value;
+  }
 
-    try {
-        exector(resolve, reject)
-    } catch (error) {
-        reject(error)
+  try {
+    exector(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
+}
+// 链式调用.then
+function resovePromise(promise2, x, resolve, reject) {
+  try {
+    if (promise2 === x) {
+      return reject(new TypeError("重复引用"));
+    } else if (
+      x !== null &&
+      (typeof x === "object" || typeof x === "function")
+    ) {
+      let then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          (r) => resolve(r),
+          (err) => reject(err)
+        );
+      } else {
+        resolve(x);
+      }
+    } else {
+      resolve(x);
     }
+  } catch (error) {
+    reject(error);
+  }
 }
 
-function rejectAll (x,resolve, reject){
-    try {
-        
-    } catch (error) {
-        
+Promise.prototype.then = function (oldResolve, oldReject) {
+  let self = this;
+  let promise2 = new Promise(function (resolve, reject) {
+    if (self.stats === "fulfilled") {
+      setTimeout(() => {
+        try {
+          let x = oldResolve(self.value);
+          resovePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
     }
-}
 
-Promise.prototype.then = function (resolve, reject) {
-    let self = this;
+    if (self.stats === "rejected") {
+      setTimeout(() => {
+        try {
+          let x = oldReject(self.value);
+          resovePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+
     // 如果是异步方法 then会先执行 需要先将函数存起来
-    if (this.stats === 'padding') {
-        this.onResolveCallBacks.push(() => resolve(self.value))
-        this.onRejectCallBacks.push(() => reject(self.value))
+    if (self.stats === "padding") {
+      try {
+        self.onResolveCallBacks.push(() => {
+          let x = oldResolve(self.value);
+          resovePromise(promise2, x, resolve, reject);
+        });
+        self.onRejectCallBacks.push(() => {
+          let x = oldReject(self.value);
+          resovePromise(promise2, x, resolve, reject);
+        });
+      } catch (error) {
+        oldReject(error);
+      }
     }
+  });
+  return promise2;
+};
 
-    if (this.stats === 'fulfilled') {
-        let x = self.value;
-        rejectAll(x,resolve, reject)
+Promise.all = function (promises) {
+  return new Promise((resove, reject) => {
+    let arr = [];
+    let index = 0;
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(
+        (res) => {
+          arr[i] = res;
+          index++;
+          if (index === promises.length) {
+            resove(arr);
+          }
+        },
+        (err) => reject(err)
+      );
     }
+  });
+};
 
-    if (this.stats === 'rejected') {
-        let x = self.value;
-        rejectAll(x,resolve, reject)
+Promise.race = function (promises) {
+  return new Promise((resolve, reject) => {
+    for (let item of promises) {
+      item.then(resolve, reject);
     }
+  });
+};
 
-
-}
-
-
-module.exports = Promise
+module.exports = Promise;
